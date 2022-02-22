4c675db998b2bc2e7a26c99a3fe41fda15c6a056
arnau.guell <arnau.guell@adevinta.com>
2022-02-22 13:13:23 +0100

    mixed solution

--- src/main/java/evaluateGuess.kt
+++ src/main/java/evaluateGuess.kt
@@ -1,39 +1,12 @@
 
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
-private const val TOTAL_MASTER_MIND_POSITIONS = 4
-// secret: ABCD, guess: AFED
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    return secret.zip(guess)
-        //[(A,A),(B,F),(C,E),(D,D)]
-        .filter { it.first != it.second }
-        //[(B,F),(C,E)]
-        .unzip()
-        //[BC,FE]
-        .let {
-            var rightPosition = TOTAL_MASTER_MIND_POSITIONS - it.first.size
-            generateEvaluationWithNumberOfAppearances(Evaluation(rightPosition, 0), it.first, it.second)
-        }
+    val rightPositions = secret.zip(guess).count { it.first == it.second }
+
+    val commonLetters = "ABCDEF".sumBy { ch ->
+
+        Math.min(secret.count { it == ch }, guess.count { it == ch })
+    }
+    return Evaluation(rightPositions, commonLetters - rightPositions)
 }
-
-private fun generateEvaluationWithNumberOfAppearances(
-    evaluation: Evaluation,
-    secret: List<Char>,
-    guess: List<Char>): Evaluation =
-
-    guess.firstOrNull()
-        .let {
-            if(it == null)
-                return evaluation
-            var position = secret.indexOf(it)
-            var secretMutable = secret.toMutableList()//Kotlin list interface not allow mutability
-            var wrongPosition = 0
-            if (position >= 0) {
-                secretMutable.removeAt(position)
-                wrongPosition++
-            }
-            // Flow control using recurrency
-            wrongPosition +=
-                generateEvaluationWithNumberOfAppearances(evaluation, secretMutable, guess.drop(1)).wrongPosition
-            Evaluation(evaluation.rightPosition, wrongPosition)
-        }
