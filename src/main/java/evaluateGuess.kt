2747c256d296c0459c63e3888388bebed5fe9328
arnau.guell <arnau.guell@adevinta.com>
2022-02-22 13:11:55 +0100

    kotlified solution

--- src/main/java/evaluateGuess.kt
+++ src/main/java/evaluateGuess.kt
@@ -2,31 +2,38 @@
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 private const val TOTAL_MASTER_MIND_POSITIONS = 4
-
+// secret: ABCD, guess: AFED
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    var guessList = guess.toCharArray().toMutableList()
-    var secretList = secret.toCharArray().toMutableList()
-    var removed: Int = 0
-    for (i in 0 until TOTAL_MASTER_MIND_POSITIONS) {
-        if (guess[i] == secret[i]) {
-            guessList.removeAt(i - removed)
-            secretList.removeAt(i - removed)
-            removed++
+    return secret.zip(guess)
+        //[(A,A),(B,F),(C,E),(D,D)]
+        .filter { it.first != it.second }
+        //[(B,F),(C,E)]
+        .unzip()
+        //[BC,FE]
+        .let {
+            var rightPosition = TOTAL_MASTER_MIND_POSITIONS - it.first.size
+            generateEvaluationWithNumberOfAppearances(Evaluation(rightPosition, 0), it.first, it.second)
         }
-    }
-
-    var rightPosition = TOTAL_MASTER_MIND_POSITIONS - secretList.size
-    var wrongPosition: Int = 0
-
-    removed = 0
-    for (i in 0 until secretList.size) {
-        var currentChar = guessList[i - removed]
-        if (secretList.contains(currentChar)) {
-            secretList.remove(currentChar)
-            guessList.remove(currentChar)
-            wrongPosition++
-            removed++
-        }
-    }
-    return Evaluation(rightPosition, wrongPosition)
 }
+
+private fun generateEvaluationWithNumberOfAppearances(
+    evaluation: Evaluation,
+    secret: List<Char>,
+    guess: List<Char>): Evaluation =
+
+    guess.firstOrNull()
+        .let {
+            if(it == null)
+                return evaluation
+            var position = secret.indexOf(it)
+            var secretMutable = secret.toMutableList()//Kotlin list interface not allow mutability
+            var wrongPosition = 0
+            if (position >= 0) {
+                secretMutable.removeAt(position)
+                wrongPosition++
+            }
+            // Flow control using recurrency
+            wrongPosition +=
+                generateEvaluationWithNumberOfAppearances(evaluation, secretMutable, guess.drop(1)).wrongPosition
+            Evaluation(evaluation.rightPosition, wrongPosition)
+        }
